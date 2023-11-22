b524681eafb2a2e17e46eab941901687add79c25
Fazila Roshan Beg <fazilaroushanbeg@gmail.com>
2023-11-22 22:19:23 +0500

    push

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:coffeedribble/util/coffee_tile.dart';
+import 'package:coffeedribble/util/coffee_type.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -10,6 +11,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //list of coffee types
+  final List coffeeType = [
+    //[coffeeType, isSelected]
+    [
+      'Cappcino',
+      true,
+    ],
+    [
+      'Latte',
+      false,
+    ],
+    [
+      'BlackCoffee',
+      false,
+    ],
+    [
+      'Tea',
+      false,
+    ],
+  ];
+
+  //user tapped on coffee types
+
+  void coffeeTypeSelected(int index) {
+    setState(() {
+      //this for loop makes every selection false
+      for (int i = 0; i < coffeeType.length; i++) {
+        coffeeType[i][1] = false;
+      }
+      coffeeType[index][1] = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -75,8 +109,25 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
 
-          //horizontal listview of coffee tiles
+          //horizontal listview of coffee types
+          Container(
+            height: 50,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: coffeeType.length,
+              itemBuilder: (context, index) {
+                return CoffeeType(
+                  coffeeType: coffeeType[index][0],
+                  isSelected: coffeeType[index][1],
+                  onTap: () {
+                    coffeeTypeSelected(index);
+                  },
+                );
+              },
+            ),
+          ),
 
+          //horizontal listview of coffee tiles
           Expanded(
             child: ListView(
               scrollDirection: Axis.horizontal,
--- lib/util/coffee_tile.dart
+++ lib/util/coffee_tile.dart
@@ -6,24 +6,71 @@ class CoffeeTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(12.0),
+      padding: const EdgeInsets.only(left: 25.0, bottom: 25, top: 25),
       child: Container(
         padding: const EdgeInsets.all(12),
         width: 200,
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
+          borderRadius: BorderRadius.circular(12),
           color: Colors.black54,
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             // coffee image
             ClipRRect(
-              borderRadius: BorderRadius.circular(16),
-              child: Image.asset('lib/images/latte.png'),
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                'lib/images/cappcino.png',
+                fit: BoxFit.fitWidth,
+              ),
             ),
 
             // coffee name
-            Text('Latte'),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 12.0, horizontal: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Latte',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    'With Almond Milk',
+                    style: TextStyle(
+                      color: Colors.grey[900],
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+            // price
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text('\$4.00'),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 4),
+                    decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: const Icon(Icons.add),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
